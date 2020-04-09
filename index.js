9eaebe06dcf169e798f290d90dcff8776cbff04f
Riaz Uddin <akreation786@gmail.com>
2020-04-09 22:07:52 +0600

    db connet, output front end and .eng envoronbment setup

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+
+require('dotenv').config()
 
 const users = ['asad', 'MOin', 'saber', 'Susmita', 'Sohana', 'sabana'];
-
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dbUser:R1811170120@cluster0-camld.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("shopStore").collection("products");
-  // perform actions on the collection object
-  collection.insertOne({
-     name: "Laptop",
-     price: 2000,
-     stock: 10
-  }, (err, res) => {
-     console.log('successfully inserted....');
-  })
-  console.log('database connected...');
-  client.close();
-});
-
-
 
 const app = express()
 
@@ -30,16 +13,30 @@ app.use(cors());
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', (req, res) =>{
-   const fruit = {
-      product: 'ada',
-      price: 200,
-   }
-   res.send(fruit)
+const dbUser = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const uri = process.env.DB_PATH;
+
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+app.get('/products', (req, res) =>{
+   client = new MongoClient(uri, { useNewUrlParser: true });
+   client.connect(err => {
+      const collection = client.db("shopStore").collection("users");
+      // collection.find({ name: "Laptop"}).toArray((err, documents) => { // for limit and filter
+      collection.find().limit(10).toArray((err, documents) => {
+            if(err){
+               console.log(err);
+               res.status(500).send({message:err})
+            }else{
+               res.send(documents)
+            }
+         });
+         client.close();
+      });
 })
-// app.get('/fruits/banana', (req, res) => {
-//    res.send({frut: 'banana', quantity: 1000, Price: 10000})
-// })
+
 
 app.get('/users/:id', (req, res)=>{
    const id = req.params.id;
@@ -48,11 +45,23 @@ app.get('/users/:id', (req, res)=>{
 });
 
 //post
-app.post('/addUser', (req, res) => {
-   //save to database
-   const user = req.body;
-   user.id = 44;
-   res.send(user);
+app.post('/addProduct', (req, res) => {
+   client = new MongoClient(uri, { useNewUrlParser: true });
+   const product = req.body;
+   
+   client.connect(errror => {
+   const collection = client.db("shopStore").collection("users");
+   collection.insertOne(product, (err, result) => {
+         if(err){
+            console.log(err);
+            res.status(500).send({message:err})
+         }else{
+            res.send(result.ops[0])
+         }
+      });
+      client.close();
+   });
 })
 
-app.listen(4000, () => console.log('Listentng to Port 4000'));
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Listentng to Port 4000'));
